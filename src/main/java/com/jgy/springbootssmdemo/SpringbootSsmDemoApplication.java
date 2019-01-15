2a8ade4ffa7d2f0ec90e16559a6a6be580a61830
Peter Ji <peterji@foxmail.com>
2019-01-15 16:41:23 +0800

    Initial commit，按照教程做了一个文字的rest api，mybatis gen装好了还没写配置文件

---
+++ src/main/java/com/jgy/springbootssmdemo/SpringbootSsmDemoApplication.java
@@ -0,0 +1,14 @@
+package com.jgy.springbootssmdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSsmDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSsmDemoApplication.class, args);
+    }
+
+}
+
